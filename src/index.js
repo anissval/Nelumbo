4aa4a6b376506b199ec8a98e5facd7fcc30b6d13
Anahi Valdiviezo <anissval@gmail.com>
2021-07-31 02:45:55 -0300

    adding firebase redux integration.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import {createFirestoreInstance} from "redux-firestore";
+import {rootReducer} from "./ducks/reducers";
+import {createStore} from "@reduxjs/toolkit";
+import firebase from "firebase/app";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import {BrowserRouter} from "react-router-dom";
+
+const rrfConfig = {
+    userProfile: "users",
+    useFirestoreForProfile: true,
+};
+
+const initialState = {};
+const store = createStore(rootReducer, initialState);
+
+const rrfProps = {
+    firebase,
+    config: rrfConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance, //since we are using Firestore
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+          <ReactReduxFirebaseProvider {...rrfProps}>
+              <BrowserRouter>
+                  <App />
+              </BrowserRouter>
+          </ReactReduxFirebaseProvider>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
